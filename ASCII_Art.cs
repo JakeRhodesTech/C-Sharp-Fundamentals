0b435f1c106b57757c727a05e8927763decba207
JakeRhodesTechUK <83876985+JakeRhodesTechUK@users.noreply.github.com>
2021-05-15 12:00:54 +0100

    Add files via upload
    First C# project with Codecademy.com, practises printing ASCII art to the console.

---
+++ ASCII_Art.cs
@@ -0,0 +1,20 @@
+﻿// ASCII Art
+
+using System;
+
+namespace ASCII_Art
+{
+    class Program
+    {
+        static void Main()
+        {
+            // prints the string to the console
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| 0 |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Percy the Poltergeist");
+        }
+    }
+}
